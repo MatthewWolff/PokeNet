bddc0e8a9e06465a144b26d20fb4d260b723423c
Matthew Wolff <mm.wolff@chi-squared.org>
2018-04-22 00:22:00 -0500

    increased readability and cleaned up methods
    Former-commit-id: c8e18df7414469621c8dd61a861cdb97104d7fac

--- poke_neural_net.r
+++ poke_neural_net.r
@@ -29,30 +29,6 @@ numerify_categorical <- function(categorical){
 binarize_categorical <- function(binary_data)
   sapply(as.logical(binary_data), function(x) if(x) 1 else -1)
 
-enumerate_categorical <- function(categorical){
-  unlist(sapply(categorical, function(x) 
-    ifelse(is.na(x), NA, which(levels(categorical) == x))
-  ))
-}
-#####################################################################
-#' @param x the index within the metadata list of the list to be stored
-#' @return None
-store_metadata <- function(x)
-  write.csv(
-    t(as.data.frame(metalists[[x]])), 
-    paste0(output,"normalized/", names(metalists[x]),".csv"),
-    row.names = FALSE, na = "")
-
-#####################################################################
-#' @param list the main list to store
-#' @param metalists columns from the main list that are separate data.frames
-#' @return None
-store <- function(list, metalists){
-  dir.create(paste0(output,"normalized/"), showWarnings = FALSE)
-  write.csv(list, paste0(output,"normalized/","main.csv"), row.names = FALSE)
-  invisible(lapply(1:length(metalists), store_metadata))
-}
-
 #####################################################################
 #' @param pairs accepts a list of pairs of pokemon IDs
 #' @param how_many how many of the pairs to expand 
@@ -73,8 +49,8 @@ make_battle <- function(pairs, how_many){
   rownames(dat) <- NULL
   return(dat)
 }
-#' @param battles a dataframe of pokemon pair statistics
-#' @param how_many the number of winners to add as a column
+#' @param battles a dataframe of pokemon pair statistics, with no winner
+#' @param how_many the number of rows of winners to add as a column
 #' @return a new dataframe that has a winner column in front!
 add_winner <- function(battles, how_many){
   winners <- as.data.frame(unname(unlist(train[3])[1:how_many]))
@@ -87,21 +63,23 @@ check_winners <- function(how_many){
   return(unname(unlist(test[3])[1:how_many]))
 }
 #####################################################################
-generate_neural_net <- function(training_set, sampling_size){
-  dat <- make_battle(training_set, sampling_size)
+#' @param sampling_size how many rows of the training set to use
+#' @return a neural net that has been trained with the specified sampling_size
+generate_neural_net <- function(sampling_size){
+  dat <- make_battle(train, sampling_size)
   dat <- add_winner(dat, sampling_size)
-  # select features and turn into formula
+  ## select features and turn into formula
   feats <- c(paste0(names(raw), 1), paste0(names(raw), 2))
   f <- paste(feats, collapse=' + ')
   f <- paste('Winner ~', f)
   f <- as.formula(f)  # Convert to formula
   
-  # run
+  ## run
   ptm <- proc.time()
   nn <- neuralnet(f, dat, hidden=c(10, 10, 10), linear.output=FALSE)
   time_taken <- proc.time() - ptm
   
-  # Check out the neural net
+  ## Check out the neural net
   # plot(nn)
   result <- paste("Net with sample of", sampling_size,"took:", time_taken[3], "seconds.\n")
   write(result, paste0(output, "performance.txt"), append = TRUE)
@@ -109,6 +87,8 @@ generate_neural_net <- function(training_set, sampling_size){
   return(nn)
 }
 
+#' @param nn a neural net that can determine the winner, given 2 pokemon IDs
+#' @return the size of training samples it had and the accuracy it could predict with
 test_neural_net <- function(nn){
   testing_size = dim(test)[1]
   testing <- make_battle(test, testing_size)
@@ -119,8 +99,8 @@ test_neural_net <- function(nn){
   cat("Neural Net of training size",length(nn$response),"\n\tAccuracy: ", acc*100, "%\n")
 }
 #####################################################################
-
-# normalizing data
+### MAIN
+## normalizing data
 pokemon <- read.csv(paste0(input,"pokemon.csv"), na.strings=c("", NA))
 pn <- pokemon %>% 
   rename(ID = X.,
@@ -143,42 +123,42 @@ pn <- pokemon %>%
          Legendary = binarize_categorical(Legendary)) %>% # convert binary to numeric
   mutate(Type2 = lapply(Type2, function(x) ifelse(!is.null(x), x, NA))) # NULL -> NA
 
-# determine which lists need to be separately stored
+## determine which lists are usable, and separate them
 lists <- pn[which(!sapply(pn, class) == "list")] # this will grab the lists and ignore lists of lists (they're bad!)
-# metalists <- pn[which(sapply(pn, class) == "list")]
-# store(lists, metalists) 
+metalists <- pn[which(sapply(pn, class) == "list")] # these needed to be encoded differently - but using them decreased accuracy 
 
-set.seed(101)
+set.seed(101) # reproducibility
 training_data <- read.csv(paste0(input,"combats.csv")) %>%
-  mutate(Winner = as.numeric(First_pokemon != Winner)) # numeric outcome
+  mutate(Winner = as.numeric(First_pokemon != Winner)) # must be numeric outcome
 
-# pull apart data into training and testing
+## pull apart data into training and testing
 raw <- lists %>% select(-c(ID, Name))
 split <- sample.split(training_data$Winner, SplitRatio = 0.20) # only leaves 10,000 training recs
 train <- subset(training_data, split == TRUE) # but we don't run more than 8,000 at a time, so
 test <- subset(training_data, split == FALSE)
 
-# parallelize neural net generation
-# no_cores <- detectCores() - 1
-# cl <- makeCluster(no_cores, type="FORK")
-# nn <- parLapply(cl, seq(5000, 5000, by=1000), 
-#                 function(x) generate_neural_net(train, x))
-# # print results && kill cluster
-# valid_net <-  13
-## test the nets. if something screws up and it's invalid, toss it out ig lol
-# invisible(sapply(nn, function(x) {if(length(x) == valid_net) test_neural_net(x)}))
-# stopCluster(cl)
+## parallelize neural net generation
+no_cores <- detectCores() - 1
+cl <- makeCluster(no_cores, type="FORK")
+sample_sizes_to_test <- seq(1000, 2000, by=1000)
+nn <- parLapply(cl, sample_sizes_to_test, function(x) generate_neural_net(x))
+
+## test the nets. 
+valid_net <-  13 # if something screws up and it's invalid, toss it out ig lol
+invisible(sapply(nn, function(x) {if(length(x) == valid_net) test_neural_net(x)}))
+stopCluster(cl)  
+
+## save results?
 # save(nn, file=paste0(output, "neural_nets.rds")) # R object version
 # dput(nn, "~/Projects/Pokemon/neural_net_object.r") # text version
                         
-# Train neural net
-nn <- generate_neural_net(train, sampling_size = 3000)
+## Train neural net on data for which we do not have results
+# nn <- generate_neural_net(sampling_size = 1000)
 
-# Generate results with it
-tests <- read.csv(paste0(input, "tests.csv"))
-tests <- make_battle(tests, "ALL")
-predicted <- compute(nn, tests)
-winners <-  sapply(predicted$net.result, round, digits=0) + 1 # turn into the index
-winning_pokemon <- as.data.frame(unlist(sapply(1:dim(tests)[1], function(x) tests[x,][winners[x]])))
-names(winning_pokemon) <- "Winner"
-write.csv(winning_pokemon, file=paste0(output, "winner.csv"))
+## Generate results for a set of test data, for which do not have results to compare with
+# tests <- make_battle(read.csv(paste0(input, "tests.csv")), "ALL")
+# predicted <- compute(nn, tests)
+# winners <-  sapply(predicted$net.result, round, digits=0) + 1 # turn into the index
+# winning_pokemon <- as.data.frame(unlist(sapply(1:dim(tests)[1], function(x) tests[x,][winners[x]])))
+# names(winning_pokemon) <- "Winner"
+# write.csv(winning_pokemon, file=paste0(output, "winner.csv"))
