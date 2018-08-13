845cfcc6fbb74f54d7b46c5d7f2fdc25f943984d
Matthew Wolff <mm.wolff@chi-squared.org>
2018-08-13 00:52:22 -0500

    cleaned up code, started adding type_matchup
    Former-commit-id: 87eb429d6b7df44dfc47d545667e767526853b60

--- poke_neural_net.r
+++ poke_neural_net.r
@@ -3,76 +3,138 @@ library(caTools)
 library(neuralnet)
 library(parallel)
 
-input <- "~/projects/pokemon/raw_data/"
-output <- "~/projects/pokemon/" # must end with a "/"
+# Customizable Constants?
+input_dir <- "~/projects/pokemon/raw_data/"
+output_dir <- "~/projects/pokemon/" # must end with a "/"
+type_matchup <- "type_matchup"
+sample_sizes_to_test <- seq(1000, 2000, by=1000)
 
 #####################################################################
-#' @param categorical a vector of categorical values that have levels
-#' @return 1-of-(C-1) effects-coding style list of vectors
-numerify_categorical <- function(categorical){
-  uniq <- unique(categorical[!is.na(categorical)]) # remove NA's for the love of god
-  categorical <- factor(categorical, levels = uniq) 
-  sorter <- lapply(uniq, function(x) {
-    rtn <- integer(length(uniq))
-    rtn[x] <- 1
-    if(as.numeric(x) == length(rtn))
-      rep(-1, length(rtn))
-    else
-      rtn
-  })
-  names(sorter) <- uniq
-  return(unname(sorter[categorical]))
-}
+# Data Cleaning/Transforming Methods 
 
 #' @param binary_data a vector of values that can be coerced to boolean
 #' @return a vector of -1 and 1's
 binarize_categorical <- function(binary_data)
   sapply(as.logical(binary_data), function(x) if(x) 1 else -1)
 
+# encodes types as an array of 1's and 0's
+#' @param type1 pokemon's primary type
+#' @param type2 pokemon's secondary type (can be NA)
+#' @return an array of 0's and 1's representing a pokemon's type(s)
+#' @note reference TYPE_LIST to translate
+encode_type <- function(type1, type2) { # NOTE: unused
+  uniq <- c(type1, type2) %>% .[!is.na(.)] %>% unique %>% sort
+  type_num <- 0:length(uniq) %>% `names<-`(c("NA", uniq)) # NA is type 0
+  
+  modify_type_array <- function(combined_type_string) { 
+    types <- strsplit(combined_type_string, " ") %>% unlist
+    type_array <- integer(length(uniq))
+    type_array[type_num[types]] <- 1 # if NA, won't get set
+    type_array 
+  }
+  encoded_types <- paste(type1, type2) %>% lapply(modify_type_array)
+  encoded_types
+}
+
+#' @param type_list1 pokemon 1's encoded type
+#' @param type_list2 pokemon 2's encoded type
+#' @return who will win and to what degree (negative indicates pokemon 1)
+determine_type_advantage <- function(type_list1, type_list2) {
+  type_effect <- function(type_list) type_chart[type_list[1], type_list[2]] # helper
+  
+  p1_types <- type_list1 %>% sapply(as.logical) %>% TYPE_LIST[.]
+  p2_types <- type_list2 %>% sapply(as.logical) %>% TYPE_LIST[.]
+  
+  get_matchup_effectiveness <- function(types1, type2) 
+    expand.grid(types1, type2) %>% split(seq(nrow(.))) %>% # cartesian product
+    lapply(unlist) %>% lapply(as.character) %>% # re-format
+    lapply(type_effect) %>% unlist %>% unname # feed into type_chart
+  
+  # sum up their possible matchups and determine who has better typing
+  p1_attacking <- get_matchup_effectiveness(p1_types, p2_types) %>% sum
+  p2_attacking <- get_matchup_effectiveness(p2_types, p1_types) %>% sum
+  
+  if (p1_attacking == p2_attacking) type_factor <- 0
+  else type_factor <- if_else(p1_attacking > p2_attacking, -1, 1)
+  type_factor*pmax(p1_attacking, p2_attacking)
+}
+
+#'@example type_chart["Fire", "Water"] -> 0.5  # not effective!
+type_chart ="
+   Normal      1.0 2.0 1.0 1.0 1.0 1.0 1.0 0.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+   Fighting    1.0 1.0 2.0 1.0 1.0 0.5 0.5 1.0 1.0 1.0 1.0 1.0 1.0 2.0 1.0 1.0 0.5 2.0
+   Flying      1.0 0.5 1.0 1.0 0.0 2.0 0.5 1.0 1.0 1.0 1.0 0.5 2.0 1.0 2.0 1.0 1.0 1.0
+   Poison      1.0 0.5 1.0 0.5 2.0 1.0 0.5 1.0 1.0 1.0 1.0 0.5 1.0 2.0 1.0 1.0 1.0 0.5
+   Ground      1.0 1.0 1.0 0.5 1.0 0.5 1.0 1.0 1.0 1.0 2.0 2.0 0.0 1.0 2.0 1.0 1.0 1.0
+   Rock        0.5 2.0 0.5 0.5 2.0 1.0 1.0 1.0 2.0 0.5 2.0 2.0 1.0 1.0 1.0 1.0 1.0 1.0
+   Bug         1.0 0.5 2.0 1.0 0.5 2.0 1.0 1.0 1.0 2.0 1.0 0.5 1.0 1.0 1.0 1.0 1.0 1.0
+   Ghost       0.0 0.0 1.0 0.5 1.0 1.0 0.5 2.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 2.0 1.0
+   Steel       0.5 2.0 0.5 0.0 2.0 0.5 0.5 1.0 0.5 2.0 1.0 0.5 1.0 0.5 0.5 0.5 1.0 0.5
+   Fire        1.0 1.0 1.0 1.0 2.0 2.0 0.5 1.0 0.5 0.5 2.0 0.5 1.0 1.0 0.5 1.0 1.0 0.5
+   Water       1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.5 0.5 0.5 2.0 2.0 1.0 0.5 1.0 1.0 1.0
+   Grass       1.0 1.0 2.0 2.0 0.5 1.0 2.0 1.0 1.0 2.0 0.5 0.5 0.5 1.0 2.0 1.0 1.0 1.0
+   Electric    1.0 1.0 0.5 1.0 2.0 1.0 1.0 1.0 0.5 1.0 1.0 1.0 0.5 1.0 1.0 1.0 1.0 1.0
+   Psychic     1.0 0.5 1.0 1.0 1.0 1.0 2.0 2.0 1.0 1.0 1.0 1.0 1.0 0.5 1.0 1.0 2.0 1.0
+   Ice         1.0 2.0 1.0 1.0 1.0 2.0 1.0 1.0 2.0 2.0 1.0 1.0 1.0 1.0 0.5 1.0 1.0 1.0
+   Dragon      1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.5 0.5 0.5 0.5 1.0 2.0 2.0 1.0 2.0
+   Dark        1.0 2.0 1.0 1.0 1.0 1.0 2.0 0.5 1.0 1.0 1.0 1.0 1.0 0.0 1.0 1.0 0.5 2.0
+   Fairy       1.0 0.5 1.0 2.0 1.0 1.0 0.5 1.0 2.0 1.0 1.0 1.0 1.0 1.0 1.0 0.0 0.5 1.0" %>% 
+  strsplit("\n") %>% sapply(strsplit, " ") %>% .[-1]  %>% sapply(function(x) x[which(x != "")]) %>%
+  as_tibble %>% `colnames<-`(.[1,]) %>% .[-1,] %>% mutate_all(as.numeric) %>% `rownames<-`(names(.)) 
+
 #####################################################################
+# Input Ingesting Methods (turning two pokemon IDs into NN data)
+
 #' @param pairs accepts a list of pairs of pokemon IDs
 #' @param how_many how many of the pairs to expand 
 #' @return a large data frame containing the stats of the pair of pokemon
-make_battle <- function(pairs, how_many){
-  if(how_many == "ALL")
-    how_many = dim(pairs)[1]
-  battles <- lapply(1:how_many, function(x){
+make_battle <- function(pairs, how_many="ALL"){
+  if(how_many == "ALL") how_many = dim(pairs)[1]
+  
+  battles <- 1:how_many %>% lapply(function(x){
     battle <- as.numeric(pairs[x,])
-    p1 <- raw[battle[1],]
-    p2 <- raw[battle[2],]
-    rtn <- c(p1, p2)
+    p1 <- stripped[battle[1],] 
+    p2 <- stripped[battle[2],]
+    c(p1, p2)
   })
-  col_names <- c(paste0(names(raw), 1), paste0(names(raw), 2))
-  dat <- as.data.frame(matrix(unlist(battles), nrow=length(unlist(battles[1]))))
-  dat <- t(dat)
+  pokemon_stats <- stripped
+  pokemon1_cols <- paste0(names(pokemon_stats), 1)
+  pokemon2_cols <- paste0(names(pokemon_stats), 2)
+  col_names <- c(pokemon1_cols, pokemon2_cols)
+  dat <- as.data.frame(matrix(unlist(battles), nrow=length(unlist(battles[1])))) %>% t
   colnames(dat) <- col_names
   rownames(dat) <- NULL
-  return(dat)
+  dat
 }
+
 #' @param battles a dataframe of pokemon pair statistics, with no winner
 #' @param how_many the number of rows of winners to add as a column
 #' @return a new dataframe that has a winner column in front!
 add_winner <- function(battles, how_many){
-  winners <- as.data.frame(unname(unlist(train[3])[1:how_many]))
+  winners <- as.data.frame(unname(train[3] %>% unlist %>% .[1:how_many]))
   colnames(winners) <- c("Winner")
   cbind(winners, battles)
 }
+
 #' @param how_many the number of winners to check
 #' @return a vector of winner outcomes... i think. it's been a while since I wrote this
 check_winners <- function(how_many){
-  return(unname(unlist(test[3])[1:how_many]))
+  test[3] %>% unlist %>% .[1:how_many]
 }
+
 #####################################################################
+# Neural Net Methods
+
 #' @param sampling_size how many rows of the training set to use
 #' @return a neural net that has been trained with the specified sampling_size
 generate_neural_net <- function(sampling_size){
   dat <- make_battle(train, sampling_size)
   dat <- add_winner(dat, sampling_size)
+  
   ## select features and turn into formula
-  feats <- c(paste0(names(raw), 1), paste0(names(raw), 2))
-  f <- paste(feats, collapse=' + ')
-  f <- paste('Winner ~', f)
-  f <- as.formula(f)  # Convert to formula
+  pokemon_stats <- stripped
+  feats <- c(paste0(names(pokemon_stats), 1), paste0(names(pokemon_stats), 2))
+  f <- paste(feats, collapse=" + ") %>% paste("Winner ~", .) %>% as.formula
   
   ## run
   ptm <- proc.time()
@@ -82,9 +144,9 @@ generate_neural_net <- function(sampling_size){
   ## Check out the neural net
   # plot(nn)
   result <- paste("Net with sample of", sampling_size,"took:", time_taken[3], "seconds.\n")
-  write(result, paste0(output, "performance.txt"), append = TRUE)
+  write(result, paste0(output_dir, "performance.txt"), append = TRUE)
   print(result)
-  return(nn)
+  nn
 }
 
 #' @param nn a neural net that can determine the winner, given 2 pokemon IDs
@@ -96,81 +158,82 @@ test_neural_net <- function(nn){
   predicted <- compute(nn, testing)
   predicted$net.result <- sapply(predicted$net.result, round, digits=0)
   acc <- sum((predicted$net.result == check_winners(testing_size)))/testing_size
-  cat("Neural Net of training size",length(nn$response),"\n\tAccuracy: ", acc*100, "%\n")
+  cat("Neural Net of training size", length(nn$response),"\n\tAccuracy: ", acc*100, "%\n")
 }
-#####################################################################
+
 #' @param sample_sizes_to_test a vector of sample sizes with which to train a neural network
 #' @return a series of neural nets, trained to the specified degrees
 parallel_train_nets <- function(sample_sizes_to_test) {
   no_cores <- detectCores() - 1
   cl <- makeCluster(no_cores, type="FORK")
-  nn <- parLapply(cl, sample_sizes_to_test, function(x) generate_neural_net(x))
+  nn <- cl %>% parLapply(sample_sizes_to_test, function(x) generate_neural_net(x))
   stopCluster(cl)
-  return(nn)
+  nn
 }
+
 #' @param nn the neural nets to test - they will be checked for validity (random corruption happens? lol)
 #' @return None. Output will be produced in stdout
 test_nets <- function(nn) {
   valid_net <-  13 # if something screws up and it's invalid, toss it out ig lol
-  invisible(sapply(nn, function(x) {if(length(x) == valid_net) test_neural_net(x)}))
+  sapply(nn, function(x) {if(length(x) == valid_net) test_neural_net(x)}) %>% invisible
 }
+
 #####################################################################
-### MAIN
-## normalizing data
-pokemon <- read.csv(paste0(input,"pokemon.csv"), na.strings=c("", NA))
-pn <- pokemon %>% 
-  rename(ID = X.,
-         Type1 = Type.1,
-         Type2 = Type.2,
-         Sp.Atk = Sp..Atk,
-         Sp.Def = Sp..Def,
-         Gen = Generation) %>%
-  mutate(Power = pmax(Attack, Sp.Atk)) %>% # Judge by their best offensive stat
-  mutate(HP = as.numeric(scale(HP)), # normalize all numerics
-         Attack = as.numeric(scale(Attack)), # assuming stats are normally distributed... 
-         Defense = as.numeric(scale(Defense)), # qqnorm looks okay, so w/e
-         Sp.Atk = as.numeric(scale(Sp.Atk)),
-         Sp.Def = as.numeric(scale(Sp.Def)),
-         Speed = as.numeric(scale(Speed)),
-         Power = as.numeric(scale(Power))) %>%
-  mutate(Type1 = numerify_categorical(Type1), # take care of categorical variables
-         Type2 = numerify_categorical(Type2),
-         Gen = numerify_categorical(Gen),
-         Legendary = binarize_categorical(Legendary)) %>% # convert binary to numeric
-  mutate(Type2 = lapply(Type2, function(x) ifelse(!is.null(x), x, NA))) # NULL -> NA
+### MAIN EXECUTION
+pokemon <- read.csv(paste0(input_dir,"pokemon.csv"), na.strings=c("", NA), stringsAsFactors = F) %>% 
+  rename(
+    ID     = X.,
+    Type1  = Type.1,
+    Type2  = Type.2,
+    Sp.Atk = Sp..Atk,
+    Sp.Def = Sp..Def,
+    Gen    = Generation
+  ) %>% as.tibble
+pn <- pokemon  %>%
+  mutate( # Judge by their best offensive stat
+    Power     = pmax(Attack, Sp.Atk) 
+  ) %>% 
+  mutate( # normalize all numerics
+    HP        = as.numeric(scale(HP)), 
+    Attack    = as.numeric(scale(Attack)), # assuming stats are normally distributed... 
+    Defense   = as.numeric(scale(Defense)), # qqnorm looks okay, so w/e
+    Sp.Atk    = as.numeric(scale(Sp.Atk)),
+    Sp.Def    = as.numeric(scale(Sp.Def)),
+    Speed     = as.numeric(scale(Speed)),
+    Power     = as.numeric(scale(Power))
+  ) %>%
+  mutate( # convert categorical variables to numerics
+    Legendary = binarize_categorical(Legendary)
+  )
+TYPE_LIST <- c(pn$Type1, pn$Type2) %>% unique %>% sort 
 
-## determine which lists are usable, and separate them
-lists <- pn[which(!sapply(pn, class) == "list")] # this will grab the lists and ignore lists of lists (they're bad!)
-metalists <- pn[which(sapply(pn, class) == "list")] # these needed to be encoded differently - but using them decreased accuracy 
-
-set.seed(101) # reproducibility
-training_data <- read.csv(paste0(input,"combats.csv")) %>%
+set.seed(101)
+training_data <- 
+  paste0(input_dir,"combats.csv") %>% read.csv %>%
   mutate(Winner = as.numeric(First_pokemon != Winner)) # must be numeric outcome
 
 ## pull apart data into training and testing
-raw <- lists %>% select(-c(ID, Name))
-split <- sample.split(training_data$Winner, SplitRatio = 0.20) # only leaves 10,000 training recs
-train <- subset(training_data, split == TRUE) # but we don't run more than 8,000 at a time, so
-test <- subset(training_data, split == FALSE)
+stripped <- pn %>% select(-c(ID, Name, Type1, Type2, Gen))
+split <- training_data$Winner %>% sample.split(SplitRatio = 0.20) # only leaves 10,000 training recs
+train <- training_data %>% subset(split == TRUE) # but we don't run more than 8,000 at a time ^, so
+test  <- training_data %>% subset(split == FALSE)
 
 ## parallelize neural net generation
-sample_sizes_to_test <- seq(1000, 2000, by=1000)
-nn <- parallel_train_nets(sample_sizes_to_test)
+nn <- sample_sizes_to_test %>% parallel_train_nets()
 
 ## test the nets. 
 test_nets(nn)
 
-## save results?
+## Save results?
 # save(nn, file=paste0(output, "neural_nets.rds")) # R object version
 # dput(nn, "~/Projects/Pokemon/neural_net_object.r") # text version
-                        
+
 ## Train neural net on data for which we do not have results
 # nn <- generate_neural_net(sampling_size = 1000)
 
-## Generate results for a set of test data, for which do not have results to compare with
-# tests <- make_battle(read.csv(paste0(input, "tests.csv")), "ALL")
+# tests <- read.csv(paste0(input, "tests.csv")) %>% make_battle() # prep data
 # predicted <- compute(nn, tests)
-# winners <-  sapply(predicted$net.result, round, digits=0) + 1 # turn into the index
+# winners <- predicted$net.result %> sapply(round, digits=0) + 1 # R-indexing starts at 1 smh
 # winning_pokemon <- as.data.frame(unlist(sapply(1:dim(tests)[1], function(x) tests[x,][winners[x]])))
 # names(winning_pokemon) <- "Winner"
 # write.csv(winning_pokemon, file=paste0(output, "winner.csv"))
